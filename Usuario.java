78e953e15f8a08cea2ddf4623f61c9be733954d1
JoselenC <choche_cr@hotmail.com>
2020-04-02 19:44:35 -0300

    Commit A

---
+++ Usuario.java
@@ -0,0 +1,16 @@
+
+public class Usuario {
+
+     private String nombre;
+     private int cedula;
+     private int contraseña;
+     private int edad;
+     private int cantHijos;
+     private int saldo=0;
+
+   
+    
+    
+    
+    
+}
