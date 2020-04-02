d0a3c81216713e6f615ca9bdc9ae4150531c059b
JoselenC <choche_cr@hotmail.com>
2020-04-02 19:48:15 -0300

    Commit I

---
+++ FuncionesUsuario.java
@@ -0,0 +1,34 @@
+package usuario;
+
+
+public class FuncionesUsuario {
+
+     Usuario u = new Usuario();
+    public double Impuestos(){
+   
+    double impuestos=0;
+    int edad= u.getEdad();
+    if((edad>18 && edad<=30) || (edad>65)){
+        impuestos=2000;
+    }
+    else if(edad>30 && edad<=65){
+        impuestos=7000;
+    }
+    else if(edad<18){
+        impuestos=0;
+    }
+    int hijos=u.getCantHijos();
+    double descontar= hijos*(0.03);
+    
+    if(descontar<=0.05){
+        impuestos=impuestos-descontar;
+    }
+    else{
+        impuestos=impuestos-0.05;
+    }
+return impuestos;
+}
+    
+  
+    
+}
